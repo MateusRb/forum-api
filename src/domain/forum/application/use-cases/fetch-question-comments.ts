ee8cd95dab59d86bc33a1fe7b299b83cee4bf3fc
Mateus <mateus_ribeiro21@hotmail.com>
2024-03-28 00:48:53 -0300

    feat: add fetch question comments use case

---
+++ src/domain/forum/application/use-cases/fetch-question-comments.ts
@@ -0,0 +1,27 @@
+import { QuestionCommentsRepository } from '@/domain/forum/application/repositories/question-comments-repository'
+import { QuestionComment } from '@/domain/forum/enterprise/entities/question-comment'
+
+interface FetchQuestionCommentsUseCaseRequest {
+  questionId: string
+  page: number
+}
+
+interface FetchQuestionCommentsUseCaseResponse {
+  questionComments: QuestionComment[]
+}
+
+export class FetchQuestionCommentsUseCase {
+  constructor(private questionCommentRepository: QuestionCommentsRepository) {}
+
+  async execute({
+    questionId,
+    page,
+  }: FetchQuestionCommentsUseCaseRequest): Promise<FetchQuestionCommentsUseCaseResponse> {
+    const questionComments =
+      await this.questionCommentRepository.findManyByQuestionId(questionId, {
+        page,
+      })
+
+    return { questionComments }
+  }
+}
