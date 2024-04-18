570ed30e5b6acaff1806314245d42d9735331298
Mateus Ribeiro <mateus_ribeiro21@hotmail.com>
2024-04-18 00:01:48 -0300

    feat: event listener for answer created

--- test/repository/in-memory-answers-repository.ts
+++ test/repository/in-memory-answers-repository.ts
@@ -1,3 +1,4 @@
+import { DomainEvents } from '@/core/events/domain-events'
 import { PaginationParams } from '@/core/repositories/pagination-params'
 import { AnswerAttachmentRepository } from '@/domain/forum/application/repositories/answer-attachments-repository'
 import { AnswersRepository } from '@/domain/forum/application/repositories/answers-repository'
@@ -31,6 +32,7 @@ export class InMemoryAnswersRepository implements AnswersRepository {
 
   async create(answer: Answer): Promise<void> {
     this.items.push(answer)
+    DomainEvents.dispatchEventsForAggregate(answer.id)
   }
 
   async delete(answer: Answer): Promise<void> {
@@ -44,5 +46,6 @@ export class InMemoryAnswersRepository implements AnswersRepository {
     const itemIndex = this.items.findIndex((item) => item.id === answer.id)
 
     this.items[itemIndex] = answer
+    DomainEvents.dispatchEventsForAggregate(answer.id)
   }
 }
